2e08060418f23812e12b9637b2c3b676d7387db9
Wihogora Wivine <wwihogora@gmail.com>
2018-12-21 17:10:14 +0200

    Add console

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,17 @@
 
 $(document).ready(function() {
     $('form').submit(function(event) {
-});
+        event.preventDefault();
+        var answers = [];
+        $('.correct').each(function () {
+            answers.push($(this).prop('checked'));
+})
+console.log(answers);
+
+
+
+
+
 
 // BUSINESS LOGIC
 
